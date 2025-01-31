c743f3627d584dd81359d8133da4f05c67be7012
Martin Poulsen <mapo0004@stud.kea.dk>
2025-01-31 11:47:33 +0100

    add
    add

--- 1._Basics.js
+++ 1._Basics.js
@@ -7,7 +7,12 @@ const lastName = "Latif";
 // EXERCISE
 // show in the console
 // My first name is Anders and my last name is Latif
-console.log("My first name is "+firstName + "and my last name is " + lastName);
+console.log("My first name is",firstName, "and my last name is", lastName);
+
+const introduction = `My first name is ${firstName} and my last name is ${lastName}`;
+console.log(introduction);
+
+//ASI 
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -18,6 +23,15 @@ const increment = 1;
 // Add the year plus the increment
 // The result should be 2025
 // You cannot touch the first or the second line
+console.log(parseInt(year) + increment);
 
+console.log(Number(year) + increment);
+
+console.log(+year+increment);
+
+const isItANumber = '1234ads'
+console.log(Number(isItANumber));
+console.log(parseInt(isItANumber));
+// TODO next week datatypes
 
 // --------------------------------------
